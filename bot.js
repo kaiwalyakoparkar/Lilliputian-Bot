e748f5ba6986a5316915f8c7bb50ea369131abf9
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-07-04 23:59:21 +0530

    chore: :art: Added comments and explaination for the logic

--- bot.js
+++ bot.js
@@ -1,17 +1,24 @@
+//Welcome to the Lilliputian Discord Bot
 console.log('Welcome to the Lilliputian Discord bot!!');
 
+//Primary Imports
 const Discord = require('discord.js');
 const client = new Discord.Client();
-require('dotenv').config();
-const fetch = require('node-fetch');
 
+//Imports which are necessary for dev
+require('dotenv').config();//required to enable use of .env file
+const fetch = require('node-fetch');//require to fetch url for use in API
+
+//Loging in as client.
 client.login(process.env.BOTTOKEN);
 
+//When client gets logged in successfully this callback runs
 client.on('ready', () => {
     console.log('Login Sucessfull');
 });
 
-
+//Points to external commands list.
 const commandHandler = require('./commands');
 
+//Access the commands in the seperate file.
 client.on('message', commandHandler);
--- commands.js
+++ commands.js
@@ -1,5 +1,7 @@
+//Fetches the module to be able to fetch API info
 const fetch = require('node-fetch');
 
+//This is the dictonery of random replies to hey message.
 const replies = [
     ':smiley::smile::heart:',
     'Hello did you call me? :thinking:',
@@ -8,26 +10,35 @@ const replies = [
 ]
 
 module.exports = async function(msg){
+
+    //This splits the sentence by spaces
     let tokens = msg.content.split(' ');
 
+    //Checks if the message is put!hey
     if(tokens[0] === 'put!hey'){
 
+        //If yes then sends in the random reply from the dictonary
         const index = Math.floor(Math.random()* replies.length);
         msg.channel.send(replies[index]);
 
+    //Checks if the message is put!gif
     }else if(tokens[0] === 'put!gif'){
 
+        //If nothing specified after the command then this is default keyword
         let keywords = 'hello'
 
+        //If Something is provided after put!gif then that is keyword
+        //eg put!gif dance here dace becomes the keyword.
         if (tokens.length > 1){
             keywords = tokens.slice(1, tokens.length).join(" ");
         }
 
-
+        //Fetches the gif through api endpoint
         let url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}&contentfilter=high`;
-        let response = await fetch(url);
-        let json = await response.json();
+        let response = await fetch(url);//Fetches the url
+        let json = await response.json();//Converts to json and sends forward
 
+        //Now this json is used to extract the actual gif url's from the returned json.
         const index = Math.floor(Math.random() * json.results.length);
         msg.channel.send(json.results[index].url);
     }
