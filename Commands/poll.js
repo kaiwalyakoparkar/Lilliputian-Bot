7d2273fe097216d7638fb52535602542b6da1547
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-07-07 10:03:15 +0530

    feat: :heavy_plus_sign: Added polling feature for individual message as well

--- Commands/poll.js
+++ Commands/poll.js
@@ -11,9 +11,17 @@ module.exports = async function (client) {
 		}, 750);
 	});
 
-	client.on('message', message => {
+	client.on('message', async (message) => {
 		if(channelIds.includes(message.channel.id)){
 			addReactions(message);
+		} else if(message.content.toLowerCase() === 'put!poll'){
+			await message.delete();
+
+			const fetched = await message.channel.messages.fetch({ limit:1 });
+
+			if(fetched && fetched.first()){
+				addReactions(fetched.first());
+			}
 		}
 	});
 }
